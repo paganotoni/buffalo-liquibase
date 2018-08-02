a5a5b392b97f9cb0107deaf3b8a6158a89bbe599
Antonio Pagano <apagano@wawand.co>
2018-08-02 18:07:46 -0500

    adding setup command

--- cmd/setup.go
+++ cmd/setup.go
@@ -1,12 +1,33 @@
 package cmd
 
-// if runtime.GOOS == "darwin" {
-// 	if _, err := exec.LookPath("brew"); err == nil {
-// 		c := exec.Command("brew", "install", "heroku")
-// 		c.Stdin = os.Stdin
-// 		c.Stderr = os.Stderr
-// 		c.Stdout = os.Stdout
-// 		return c.Run()
-// 	}
-// }
-// return errors.New("heroku cli is not installed. https://devcenter.heroku.com/articles/heroku-cli")
+import (
+	"errors"
+	"os"
+	"os/exec"
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
+
+// setupCmd translates fizz into liquibase
+var setupCmd = &cobra.Command{
+	Use:   "setup",
+	Short: "description about this plugin",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if runtime.GOOS == "darwin" {
+			if _, err := exec.LookPath("brew"); err == nil {
+				c := exec.Command("brew", "install", "liquibase")
+				c.Stdin = os.Stdin
+				c.Stderr = os.Stderr
+				c.Stdout = os.Stdout
+				return c.Run()
+			}
+		}
+
+		return errors.New("Could not install liquibase cli with brew, please install manually, for instructions visit https://www.liquibase.org/")
+	},
+}
+
+func init() {
+	liquibaseCmd.AddCommand(setupCmd)
+}
