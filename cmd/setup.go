a49db1ac5e21ffbc4f1b2845aae91936bf86c41e
Antonio Pagano <apagano@wawand.co>
2018-08-02 18:15:26 -0500

    simplifying setup

--- cmd/setup.go
+++ cmd/setup.go
@@ -14,17 +14,24 @@ var setupCmd = &cobra.Command{
 	Use:   "setup",
 	Short: "description about this plugin",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if runtime.GOOS == "darwin" {
-			if _, err := exec.LookPath("brew"); err == nil {
-				c := exec.Command("brew", "install", "liquibase")
-				c.Stdin = os.Stdin
-				c.Stderr = os.Stderr
-				c.Stdout = os.Stdout
-				return c.Run()
-			}
+		if _, err := exec.LookPath("liquibase"); err == nil {
+			cmd.Printf("Found liquibase, all good.")
 		}
 
-		return errors.New("Could not install liquibase cli with brew, please install manually, for instructions visit https://www.liquibase.org/")
+		if runtime.GOOS != "darwin" {
+			return errors.New("Could not find liquibase, please install manually, for instructions visit https://www.liquibase.org/")
+		}
+
+		if _, err := exec.LookPath("brew"); err != nil {
+			return errors.New("Could not find liquibase or brew, please install manually, for instructions visit https://www.liquibase.org/")
+		}
+
+		c := exec.Command("brew", "install", "liquibase")
+		c.Stdin = os.Stdin
+		c.Stderr = os.Stderr
+		c.Stdout = os.Stdout
+		return c.Run()
+
 	},
 }
 
